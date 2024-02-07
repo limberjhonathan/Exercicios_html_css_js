74cc46b4021d58f25976203a35a6bee6e83a2d17
Limber Jhonathan <limberjhonathan@gmail.com>
2024-02-06 23:23:58 -0300

    disabled input

--- lista_compras/script.js
+++ lista_compras/script.js
@@ -1,19 +1,30 @@
-function Compras(){
+function Compras() {
+    const produto = document.querySelector("#produto");
+    const quantidade = document.querySelector("#quantidade");
+    const preco_uni = document.querySelector("#preco_uni");
+    const btn_off = document.querySelector(".btn_off");
+    const btn_conf = document.querySelector(".btn_conf");
+
     this.inicio = () => {
-        this.detectarClique()
-    }
+        this.detectarMudanca();
+    };
 
-    this.detectarClique = () => {
-        const btn = document.querySelector(".btn_conf")
-        btn.addEventListener("click", this.comprasInseridas)
-    }
+    this.detectarMudanca = () => {
+        produto.addEventListener("input", this.verificarCampos);
+        quantidade.addEventListener("input", this.verificarCampos);
+        preco_uni.addEventListener("input", this.verificarCampos);
+    };
 
-    this.comprasInseridas = () => {
-        const produto = document.querySelector("#produto")
-        const quantidade = document.querySelector("#quantidade")
-        const preco_uni = document.querySelector("#preco_uni")
-    }
+    this.verificarCampos = () => {
+        if (produto.value.trim() !== '' && quantidade.value.trim() !== '' && preco_uni.value.trim() !== '') {
+            btn_off.disabled = false;
+            btn_conf.classList.add('enabled');
+        } else {
+            btn_off.disabled = true;
+            btn_conf.classList.remove('enabled');
+        }
+    };
 }
 
-const compras = new Compras()
-compras.inicio()
+const compras = new Compras();
+compras.inicio();
