bc14f9fbab42048f3d41f2d1acdf5be99f8b5723
Limber Jhonathan <limberjhonathan@gmail.com>
2024-02-22 23:54:50 -0300

    login finalizado

--- login_black/script.js
+++ login_black/script.js
@@ -1,14 +1,20 @@
-class Eye{
-    constructor(){
-        this.eye = document.querySelector("#eye")
+class Eye {
+    constructor() {
+        this.eye = document.querySelector("#eye");
+        this.pass = document.querySelector("#password");
+        this.togglePasswordVisibility = this.togglePasswordVisibility.bind(this);
+        this.eye.addEventListener("click", this.togglePasswordVisibility);
     }
-        
-    cliqueEye(){
-        this.eye.addEventListener("click", () => {
-            alert("ok")
-        })
+
+    togglePasswordVisibility() {
+        if (this.eye.classList.contains("bi-eye-fill")) {
+            this.eye.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
+            this.pass.type = "text";
+        } else {
+            this.pass.type = "password";
+            this.eye.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
+        }
     }
 }
 
-const e = new Eye();
-e.cliqueEye()
+const eyeInstance = new Eye();
