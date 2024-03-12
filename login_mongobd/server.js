f49ad8b3d3b02e01e4033c1f3c4f5b59e29b006c
Limber Jhonathan <limberjhonathan@gmail.com>
2024-03-11 23:32:50 -0300

    mongoose

--- login_mongobd/server.js
+++ login_mongobd/server.js
@@ -1,13 +1,36 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.emit('pronto');
+  })
+  .catch(e => console.log(e));
+
 const path = require('path');
 const routes = require("./routes");
 const { checkPageExistence, errorHandler } = require("./src/middlewares/middlewares");
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 app.use(express.urlencoded({ extended: true }));
 
 // Configuração dos arquivos estáticos
 app.use(express.static(path.resolve(__dirname, "public")));
+
+const sessionOptions = session({
+  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
+});
 
 // Configurar o mecanismo de visualização e o diretório de visualizações
 app.set('views', path.resolve(__dirname, 'src', 'views'));
