ebe37e34445b2c6e5f922ed4623576593dbcd5bd
Limber Jhonathan <limberjhonathan@gmail.com>
2024-03-13 00:01:15 -0300

    implementação de comparação de senhas

--- login_mongobd/public/js/register.js
+++ login_mongobd/public/js/register.js
@@ -3,11 +3,17 @@ class RegisterPassword {
         this.passwordIcon = document.querySelector("#password_icon");
         this.repeatPasswordIcon = document.querySelector("#repeat_password_icon");
         this.passwordInputs = document.querySelectorAll(".password");
+        this.registerForm = document.querySelector(".registerForm");
+        this.span = document.querySelector(".span");
     }
 
     clique = () => {
         this.passwordIcon.addEventListener("click", this.passwordVisible);
         this.repeatPasswordIcon.addEventListener("click", this.repeatPasswordVisible);
+        this.registerForm.addEventListener("submit", this.comparePassword)
+        this.passwordInputs.forEach(input => {
+            input.addEventListener('mousedown', this.inputPadlock)
+        })
     }
 
     passwordVisible = () => {
@@ -29,6 +35,24 @@ class RegisterPassword {
             this.passwordInputs[1].setAttribute("type", "password");
         }
     }
+
+    comparePassword = (event) => {
+        if (this.passwordInputs[0].value !== this.passwordInputs[1].value){
+            this.passwordInputs[1].style.border = '2px solid red'
+            this.span.style.display = 'block'
+            event.preventDefault();
+        }
+    }
+
+    inputPadlock = (event) => {
+        const clickedInput = event.target;
+        if (clickedInput.id === "password1") {
+            console.log("Clicou no primeiro campo de senha");
+        } else if (clickedInput.id === "password2") {
+            console.log("Clicou no segundo campo de senha");
+        }
+    }
+    
 }
 
 const registerpass = new RegisterPassword
